c2152cc9c3a234ea3201137a6342bf75859a24a2
Serg Mykhailov <serg.mykhailov@gmail.com>
2024-05-20 13:00:00 +0300

    Implement names validation service

--- names_validator_service.py
+++ names_validator_service.py
@@ -1,1 +1,49 @@
-# TODO: IMPLEMENT
+try:
+  import flask
+except ImportError:
+  try:
+    import pip
+    pip.main(['install', 'flask'])
+  except Exception as e:
+    print(e)
+
+try:
+    import names_dataset
+except ImportError:
+    import pip
+    pip.main(['install', 'names_dataset'])
+
+
+from names_dataset import NameDataset
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+names_dataset = NameDataset()
+
+
+@app.route("/")
+def hello_world():
+    name = request.args.get('name', '')
+    if name is None or not isinstance(name, str) or len(name) == 0:
+        return jsonify({})
+    
+    response = {}
+    try:
+        name_info = names_dataset.search(name)
+        response = {
+           'status': 'succeeded',
+           'data': name_info
+        }
+    except Exception as e:
+        response = {
+          'status': 'failed',
+          'reason': str(e)
+        }
+
+    response_data = jsonify(response)
+    return response_data
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
